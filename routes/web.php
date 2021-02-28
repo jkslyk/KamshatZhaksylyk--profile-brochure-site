a5b5d82f615fa998878478274e780050ab4b219d
190103366@stu.sdu.edu.kz <190103366@stu.sdu.edu.kz>
2021-02-28 18:30:47 +0600

    create post table

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
+
+use App\Models\Client;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,3 +31,16 @@ Route::get('/about', function() {
 Route::get('/contact', function() {
     return view('contact');
 })->name('contact');
+
+Route::get('client/add', function() {
+    DB::table('clients')->insert([
+        'name' => 'Kamshat',
+        'surname' => 'Zhaksylyk',
+        'age' => 18
+    ]);
+});
+
+Route::get('client', function() {
+    $client = Client::find(1);
+    return $client;
+});
