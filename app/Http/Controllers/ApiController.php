098dd5c1aa0deaa7f7e799c95d5b7a2dd03faacb
190103366@stu.sdu.edu.kz <190103366@stu.sdu.edu.kz>
2021-03-16 19:45:12 +0600

    new Route to Api.php, ApiController

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Post;
+
+class ApiController extends Controller
+{
+    /* 
+        This function will return all posts in my database.
+    */
+
+    public function index(Request $request) {
+        $posts = Post::all();
+
+        return response($posts, 200);
+    }
+
+    /*
+        Return post with post_id
+    */
+    public function get_post(Request $request) {
+        $post = Post::find($request->post_id);
+
+        if($post == null) {
+            return response(['message' => 'There is no post'],404);
+        }
+
+        return response($post, 200);
+    }
+}
